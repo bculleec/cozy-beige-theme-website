b422976f95d54f1ed576eba083e5dff5b35a0e9b
bculleec <145722460+bculleec@users.noreply.github.com>
2023-12-29 21:28:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function OpenVSCodeLink() {
+    window.open('https://marketplace.visualstudio.com/items?itemName=bculleec.cozy-beige', '_blank');
+}
+
+function OpenPortfolio() {
+    window.open('https://bculleec.github.io/', '_blank');
+}
+
+function OpenProjectGitHub() {
+    window.open('https://github.com/bculleec/cozybeige-theme', '_blank');
+}
